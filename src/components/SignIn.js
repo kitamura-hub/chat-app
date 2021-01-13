80016104bd2be2aa4d3a6845825f3f90e0a741a3
kitamura-hub <masato1999824@icloud.com>
2021-01-13 17:19:28 +0900

    日本語入力の編集中かどうかを判定する処理を加え、enterログイン時の挙動を改善

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -50,6 +50,7 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [ disabled, setDisabled ] = useState(true);  //はじめるボタンの開閉状態を管理(trueは閉｜falseは開)
   const [ string, setString ] = useState('');  //ニックネームの入力値を管理
+  const [ isEdit, setIsEdit ] = useState(false);// 日本語の編集中かどうかの判定(状態)を管理(trueは編集中｜falseは編集中ではない)
 
   // string(入力値)を監視して、変化がある度に中の処理を実行
   useEffect(() => {
@@ -76,11 +77,16 @@ export default function SignIn({ setName }) {
             autoFocus
             onChange={(e) => setString(e.target.value)}
             onKeyDown={(e) => {
+              if (isEdit) return;  // isEditがtrue(編集中であれば)何も返さない(ここで処理を終了させる。早期return)
               if (e.key === "Enter") {
                 e.preventDefault();
                 setName(e.target.value);  // 引数はstring(state)でも可
               }
             }}
+            // 日本語の変換処理がスタートしたときに発火
+            onCompositionStart={(e) => setIsEdit(true)}
+            // 日本語入力の変換が確定したときに発火
+            onCompositionEnd={(e) => setIsEdit(false)}
           />
           <Button
             type="button"
