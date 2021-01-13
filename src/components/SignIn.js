0f1b5140cfaa747833aa83e2d5c67695cb14047e
kitamura-hub <masato1999824@icloud.com>
2021-01-13 14:53:15 +0900

    <Improve>: ボタンクリック時のリロードを無効化して、入力値をname(state)にセットするまでを実装

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -77,12 +77,13 @@ export default function SignIn({ setName }) {
             onChange={(e) => setString(e.target.value)}
           />
           <Button
-            type="submit"
+            type="button"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
             disabled={disabled}
+            onClick={() => setName(string)}
           >
             はじめる
           </Button>
