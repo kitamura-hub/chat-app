f6623be3c989c60a74cd6dc388e4b3ca3395d40d
kitamura-hub <masato1999824@icloud.com>
2021-01-14 15:20:21 +0900

    <Add>: Main.jsで使用している子コンポーネントにもGridを適用しました

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    gridRow: 2,  // MessageInputFieldはGridにおいて、Gridが定める境界線が上から数えて2番目に位置する
+  }
+});
 
 const MessageInputField = () => {
-  return <div>MessageInputField</div>;
+  const classes = useStyles();
+  return <div className={classes.root}>MessageInputField</div>;
 }
 export default MessageInputField;
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    gridRow: 1,  // MessageInputFieldはGridにおいて、Gridが定める境界線が上から数えて1番目に位置する
+  }
+});
 
 const MessageList = () => {
-  return <div>MessageList</div>;
+  const classes = useStyles();
+  return <div className={classes.root}>MessageList</div>;
 }
 export default MessageList;
