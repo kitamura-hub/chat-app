7d39b395e0e0d688d7786d5496dbf521b836fcf1
kitamura-hub <masato1999824@icloud.com>
2021-01-14 11:58:45 +0900

    <Add>: ルーティング制御用のconfigファイルを作成し、一時的にルーティングを制御しました

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 
 import Main from './Main';
 import SignIn from './SignIn';
+import config from '../config.json';
 
 export default () => {
   const [ name, setName ] = useState('');
 
-  // name(ニックネームの入力値)によって返すコンポーネントを制御
-  if (name === "") {
+  // ルーティング制御
+  if (config.signInEnabled && name === "") {
     return <SignIn setName={setName} />;
   } else {
     return <Main name={name} />;
